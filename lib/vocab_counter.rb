c1e54873489a9fb00ea09581ee7e431dc40f0f30
Julian Burgess <aubergene@gmail.com>
2009-11-07 22:47:50 +0000

    Changed to work better with standard unix pipes

--- lib/vocab_counter.rb
+++ lib/vocab_counter.rb
@@ -1,16 +1,21 @@
 class VocabCounter
 
-  def self.count(input)
-    input.downcase!
-    input.gsub!(/[^a-z0-9']/, ' ')
-    input.gsub!(/\s'|'\s/, ' ')
-    count = Hash.new(0)
-    input.split(/\s+/).each do |term|
-      count[term.to_sym] += 1
-    end
+  def initialize
+    @count = Hash.new(0)
+  end
 
+  def count(line)
+    line.downcase!
+    line.gsub!(/[^a-z0-9']/, ' ')
+    line.gsub!(/\s'|'\s/, ' ')
+    line.split(/\s+/).each do |term|
+      @count[term.to_sym] += 1
+    end
+  end
+
+  def report
     out = ""
-    count.sort { |a,b| b[1] <=> a[1] }.each do |k,v|
+    @count.sort { |a,b| b[1] <=> a[1] }.each do |k,v|
       out << "#{k.to_s},#{v}\n"
     end
     out
